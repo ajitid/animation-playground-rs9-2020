b62676b9874f21136a32e25b428d21dafd8cea2b
Ajit Singh <ajit.singh@ecs.co.uk>
2020-06-24 14:52:42 +0530

    add comments

--- src/wannabe-samosa/utils/DecelerationRate.ts
+++ src/wannabe-samosa/utils/DecelerationRate.ts
@@ -1,5 +1,7 @@
 // Values obtained from https://medium.com/@esskeetit/scrolling-mechanics-of-uiscrollview-142adee1142c
 // confirmed from https://stackoverflow.com/a/8711525
+// useful to calcuate projected position when used with `project`
+// smooth scroll, where it'll end and flick end stuff
 enum DecelerationRate {
   Normal = 0.998,
   Fast = 0.99,
