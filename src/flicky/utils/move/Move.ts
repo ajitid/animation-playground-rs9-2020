394e221b116f3aec73c9222d50eec2b38d43dce4
Ajit Singh <ajit.singh@ecs.co.uk>
2020-07-05 21:08:10 +0530

    make Move working

--- src/flicky/utils/move/Move.ts
+++ src/flicky/utils/move/Move.ts
@@ -3,22 +3,26 @@ import React from 'react';
 import { StyleProps, MoveStyles } from './types';
 import useMove, { UseMoveShape } from './useMove';
 
-type PickedUseMoveShape = Omit<UseMoveShape, 'addStyles'>;
+type PickedUseMoveShape = Omit<UseMoveShape, 'addStyles' | 'key'>;
 
 interface MoveProps extends PickedUseMoveShape {
+  moveKey: string;
   style?: StyleProps;
 }
 
-const Move: React.FC<MoveProps> = ({ children, style, ...props }) => {
+const Move: React.FC<MoveProps> = ({ children, style, moveKey, ...props }) => {
   const child = React.Children.only(children);
 
-  const childProps = useMove({ ...props, addStyles: style as MoveStyles });
+  const childProps = useMove({ ...props, key: moveKey, addStyles: style as MoveStyles });
 
   if (!React.isValidElement(child)) return null;
 
   return React.cloneElement(child, {
-    childProps,
-    style,
+    ...childProps,
+    style: {
+      ...style,
+      ...childProps.style,
+    },
   });
 };
 
--- src/pages/Stuff2.tsx
+++ src/pages/Stuff2.tsx
@@ -2,26 +2,17 @@ import React, { useState, useRef, RefObject } from 'react';
 import { a } from '@react-spring/web';
 
 import DefaultLayout from 'layouts/DefaultLayout';
-
-import useMove from '../flicky/utils/move/useMove';
 import Link from 'elements/atoms/Link';
+import Move from '../flicky/utils/move/Move';
 
 const Stuff2 = () => {
-  // const boxRef = useRef(null);
-  const { style: moveStyles, ref: boxRef } = useMove<HTMLDivElement>({
-    id: 'box',
-    key: '2',
-  });
-
   return (
     <DefaultLayout pageTitle="Stuff">
       <div className="container mx-auto pt-4">
         <Link to="stuff">stuff1</Link>
-        <a.div
-          ref={boxRef}
-          style={{ ...moveStyles, transformOrigin: '0% 0%' }}
-          className="w-6 h-4 bg-purple-300"
-        ></a.div>
+        <Move id="box" moveKey="2" style={{ transformOrigin: '0% 0%' }}>
+          <a.div className="w-6 h-4 bg-purple-300"></a.div>
+        </Move>
       </div>
     </DefaultLayout>
   );
