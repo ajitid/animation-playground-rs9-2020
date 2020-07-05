c7c1305b1744a1740b16381b15300ad118f05bc5
Ajit Singh <ajit.singh@ecs.co.uk>
2020-07-05 11:59:03 +0530

    multiple page magic move but broken single page

--- src/flicky/utils/move/useMove.ts
+++ src/flicky/utils/move/useMove.ts
@@ -1,18 +1,18 @@
-import { useLayoutEffect, RefObject } from 'react';
+import { useLayoutEffect, RefObject, useContext, useEffect } from 'react';
 import { useSpring } from '@react-spring/web';
-import usePreviousValue from 'hooks/usePreviousValue';
 
-interface Position {
-  left: number;
-  top: number;
-  width: number;
-  height: number;
+import { Position } from './types';
+import usePreviousValue from 'hooks/usePreviousValue';
+import MoveContext from './MoveContext';
+
+interface UseMoveShape<T extends HTMLElement = HTMLElement> {
+  id: string;
+  ref: RefObject<T>;
+  key: any;
 }
 
-const useMagicMotion = <T extends HTMLElement>(ref: RefObject<T>) => {
-  const cachedPosition = usePreviousValue<Position | null>(
-    ref.current?.getBoundingClientRect() ?? null
-  );
+const useMove = ({ id, ref, key }: UseMoveShape) => {
+  const { getCachedPosition, updateCachedPosition } = useContext(MoveContext);
 
   const [styles, set] = useSpring(() => ({
     x: 0,
@@ -20,15 +20,24 @@ const useMagicMotion = <T extends HTMLElement>(ref: RefObject<T>) => {
     scaleX: 1,
     scaleY: 1,
     config: {
-      frequency: 0.7,
+      frequency: 3,
       damping: 0.9,
     },
   }));
 
+  // anim on key change
   useLayoutEffect(() => {
-    if (!ref.current) return;
-    if (cachedPosition === null) return;
-    const newPosition = ref.current.getBoundingClientRect();
+    const cachedPosition = getCachedPosition(id);
+    if (ref.current === null || cachedPosition === null) return;
+
+    const newPositionDomRect = ref.current.getBoundingClientRect();
+    const newPosition = {
+      height: newPositionDomRect.height,
+      left: newPositionDomRect.left,
+      top: newPositionDomRect.top,
+      width: newPositionDomRect.width,
+    };
+    console.log('happened', cachedPosition, newPosition);
 
     set({
       from: {
@@ -42,9 +51,27 @@ const useMagicMotion = <T extends HTMLElement>(ref: RefObject<T>) => {
       scaleX: 1,
       scaleY: 1,
     });
-  });
+  }, [key]); /* eslint-disable-line react-hooks/exhaustive-deps */
+
+  // unmount read
+  useEffect(() => {
+    const position = ref.current?.getBoundingClientRect(); /* eslint-disable-line react-hooks/exhaustive-deps */
+    if (!position) return;
+    const { height, left, top, width } = position;
+    updateCachedPosition(id, { height, left, top, width });
+  }, [key]); /* eslint-disable-line react-hooks/exhaustive-deps */
+
+  // // unmount read
+  // useEffect(() => {
+  //   return () => {
+  //     const position = ref.current?.getBoundingClientRect(); /* eslint-disable-line react-hooks/exhaustive-deps */
+  //     if (!position) return;
+  //     const { height, left, top, width } = position;
+  //     updateCachedPosition(id, { height, left, top, width });
+  //   };
+  // }, []); /* eslint-disable-line react-hooks/exhaustive-deps */
 
   return styles;
 };
 
-export default useMagicMotion;
+export default useMove;
