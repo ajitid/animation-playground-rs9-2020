96b8f4d6f11b0635f076deb47808a8bcbfd27e67
Ajit Singh <ajit.singh@ecs.co.uk>
2020-07-06 00:33:47 +0530

    improve styles

--- src/pages/Stuff2.tsx
+++ src/pages/Stuff2.tsx
@@ -8,10 +8,18 @@ import Move from '../flicky/utils/move/Move';
 const Stuff2 = () => {
   const [boxes, setBoxes] = useState([1, 2, 3, 4]);
 
+  const colors: Record<number, string> = {
+    1: 'lightgreen',
+    2: 'pink',
+    3: 'orange',
+    4: 'lightblue',
+  };
+
   return (
     <DefaultLayout pageTitle="Stuff">
       <div className="container mx-auto pt-4">
         <button
+          className="mb-4 px-2 py-1 bg-gray-300 rounded"
           onClick={() => {
             setBoxes(prevBoxes => {
               const boxes = [...prevBoxes];
@@ -20,11 +28,16 @@ const Stuff2 = () => {
             });
           }}
         >
-          remove
+          Remove first box
         </button>
         <div className="flex justify-between">
           {boxes.map(box => (
-            <Move key={box} id={box} moveKey={boxes.length}>
+            <Move
+              key={box}
+              id={box}
+              moveKey={boxes.length}
+              style={{ backgroundColor: colors[box] }}
+            >
               <a.div className="w-16 h-16 bg-gray-400" />
             </Move>
           ))}
