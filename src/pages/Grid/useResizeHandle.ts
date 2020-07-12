b9273c21a3aceb8ddd50d2109636de159fd833eb
Ajit Singh <ajit.singh@ecs.co.uk>
2020-07-12 12:27:47 +0530

    cleanup

--- src/pages/Grid/useResizeHandle.ts
+++ src/pages/Grid/useResizeHandle.ts
@@ -6,17 +6,6 @@ const useResizeHandle = (
   containerRef: RefObject<HTMLElement>,
   onResizeDone = noop
 ) => {
-  // const capturedHandleBoundsRef = useRef<RectReadOnly>({
-  //   bottom: 0,
-  //   height: 0,
-  //   left: 0,
-  //   right: 0,
-  //   top: 0,
-  //   width: 0,
-  //   x: 0,
-  //   y: 0,
-  // });
-
   useEffect(() => {
     const handle = handleRef.current;
     const container = containerRef.current;
