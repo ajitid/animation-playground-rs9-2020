937a70e211022937cab9b008aee485be0b3436dd
Ajit Singh <ajit.singh@ecs.co.uk>
2020-07-13 15:07:51 +0530

    clamp

--- src/pages/Grid/useScaleWithItem.ts
+++ src/pages/Grid/useScaleWithItem.ts
@@ -2,6 +2,7 @@ import { useEffect, useContext, RefObject } from 'react';
 
 import { PackingGridContext } from './PackingGrid';
 import { ItemContext } from './Item';
+import clamp from 'flicky/utils/clamp';
 
 const useScaleWithItem = (itemContentBlockRef: RefObject<HTMLElement>) => {
   const { relayout, cols, gridWidth } = useContext(PackingGridContext);
@@ -12,10 +13,11 @@ const useScaleWithItem = (itemContentBlockRef: RefObject<HTMLElement>) => {
     const itemContent = itemContentBlockRef.current;
     if (!item || !itemContent || gridWidth === 0) return;
 
+    const clampedXUnit = clamp(1, cols, xUnit);
     const excessWidth =
       parseFloat(getComputedStyle(item).marginLeft.replace('px', '')) +
       parseFloat(getComputedStyle(item).marginRight.replace('px', ''));
-    itemContent.style.width = `${(gridWidth / cols) * xUnit - excessWidth}px`;
+    itemContent.style.width = `${(gridWidth / cols) * clampedXUnit - excessWidth}px`;
     relayout();
   }, [cols, itemContentBlockRef, gridWidth, relayout, itemRef, xUnit]);
 };
